795f5c41f4da078729086ff1c753a30f1b43846f
Lotte <joshdoes3d@gmail.com>
2025-09-01 13:40:38 +1000

    Revert Unpack due to NTFS

--- src/com/formdev/flatlaf/util/LoggingFacade.java
+++
@@ -1,9 +0,0 @@
-package com.formdev.flatlaf.util;
-
-public interface LoggingFacade {
-   LoggingFacade INSTANCE = new LoggingFacadeImpl();
-
-   void logSevere(String var1, Throwable var2);
-
-   void logConfig(String var1, Throwable var2);
-}
