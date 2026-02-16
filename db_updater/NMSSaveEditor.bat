d0150ec70b559ffe6598a4b57fca2da3dcc5ecf4
Josh (vector_cmdr) <joshdoes3d@gmail.com>
2026-02-17 03:35:13 +1100

    Update 1.20.1a
    Reverted bat changes for new JRE native support.

--- db_updater/NMSSaveEditor.bat
+++ db_updater/NMSSaveEditor.bat
@@ -1,7 +1,7 @@
 @echo off
 :start
 ::Add --enable-native-access=ALL-UNNAMED to support flatlaf (due to aging API)
-java --enable-native-access=ALL-UNNAMED -jar NMSSaveEditor.jar -autoupdate
+java -jar NMSSaveEditor.jar -autoupdate
 if errorLevel 2 (
 	move /Y ~NMSSaveEditor.dl NMSSaveEditor.jar
 	goto start
